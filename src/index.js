93d735d80c1ddcb10d262d5d8b663221a5088711
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-03-26 20:56:21 +0300

    add index.js and get-refs.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
+import { getRefs } from './get-refs';
 import './css/styles.css';
 
+const refs = getRefs();
+
+// songs name
+const songs = [
+  'Артем Пивоваров and NK - Там У Тополi (Roman Crash Remix)',
+  'Хас feat Схожа - Не продам',
+  'Cheev - Гарно так',
+  'MBreeze - Дихай',
+  'Shumei - Біля тополі',
+];
+
+// default song
+let currentSong = 0;
+
+// load song
+function loadSong(song) {
+  refs.songName.innerHTML = song;
+  refs.audio.src = `songs/${song}.mp3`;
+}
+
+loadSong(songs[currentSong]);
+
+// play song
+function playSong() {
+  refs.audio.play();
+}
+
+// pause song
+function pauseSong() {
+  refs.audio.pause();
+}
+
+// event listener
+refs.btnPlay.addEventListener('click', () => {
+  playSong();
+});
